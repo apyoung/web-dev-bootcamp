b3d55af426112841ff2b6d178804663d95eec06c
Andrew Young <apyoung88@gmail.com>
2019-10-08 16:59:55 -0400

    Make progress in section 15

---
+++ Section 15 - DOM Manipulation/selectorMethods.js
@@ -0,0 +1,12 @@
+var tag = document.getElementById("highlight");
+
+var tags = document.getElementsByClassName("bolded");
+console.log(tags[0]);
+
+var tags = document.getElementsByTagName("li");
+console.log(tags[0]);
+
+var body = document.getElementsByTagName("body");
+
+var li = document.querySelector("#highlight");
+
