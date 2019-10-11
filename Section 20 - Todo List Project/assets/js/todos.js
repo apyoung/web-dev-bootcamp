ba70b0bf96828c840e49487d071439ca6d6394bc
MooseMachine <apyoung88@gmail.com>
2019-10-10 22:24:36 -0400

    Complete section 20

--- Section 20 - Todo List Project/assets/js/todos.js
+++ Section 20 - Todo List Project/assets/js/todos.js
@@ -1,15 +1,31 @@
 //cross out todos by clicking
-$("li").click(function ()
+$("ul").on("click", "li", function ()
 {
     $(this).toggleClass("completed");
 });
 
 //click on X to delete ToDo
-$("span").click(function (event)
+$("ul").on("click", "span", function (event)
 {
-   event.stopPropagation();
-   $(this).parent().fadeOut(500, function ()
-   {
-       $(this).remove();
-   });
+    event.stopPropagation();
+    $(this).parent().fadeOut(500, function ()
+    {
+        $(this).remove();
+    });
 });
+
+$("input[type='text']").keypress(function (event)
+{
+    if (event.which === 13)
+    {
+        var todoText = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span><i class=\"fa fa-trash\"></i></span> " + todoText + "</li>");
+    }
+});
+
+//need to look at solution code to get working
+$(".fa-plus").click(function ()
+{
+    $("input[type='text']").fadeOut();
+})
